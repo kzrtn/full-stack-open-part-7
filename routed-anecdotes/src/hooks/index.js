13eeb7ab176bcbd1a58102fa2f239593d8e3c094
Jolene T <kartistenn2@gmail.com>
2026-09-12 07:21:52 +0000

    added ability to create new anecdotes in hook, connected to backend

--- routed-anecdotes/src/hooks/index.js
+++ routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import anecdoteService from '../services/anecdotes.js'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
@@ -21,6 +22,21 @@ export const useField = (type) => {
   }
 }
 
-export const useAnotherHook = () => {
-  // ...
+export const useAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = useState([])
+
+  useEffect(() => {
+    anecdoteService.getAll().then(data => setAnecdotes(data))
+  }, [])
+
+  const addAnecdote = (object) => {
+    anecdoteService.createNew(object).then(data => {
+      setAnecdotes(anecdotes.concat(data))
+    })
+  }
+  
+  return {
+    anecdotes,
+    addAnecdote
+  }
 }
