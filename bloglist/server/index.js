79b4f79d3549c942940b2e8230bc7727f61efc2d
Jolene T <kartistenn2@gmail.com>
2026-09-14 04:45:08 +0000

    added using dist for prod

--- bloglist/server/index.js
+++ bloglist/server/index.js
@@ -1,6 +1,15 @@
+const express = require('express')
+const path = require('path')
 const app = require('./app.js')
 const config = require('./utils/config.js')
 const logger = require('./utils/logger.js')
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')))
+  app.get('/*splat', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  })
+}
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
